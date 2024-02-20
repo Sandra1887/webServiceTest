58f1f955af49e2b486d2d4c2a75907246a0118c5
Sandra1887 <93sanjep@molndal.gafe.se>
2024-02-20 08:51:50 +0100

    init message

---
+++ src/main/java/com/example/awsServiceDemoTest2/AwsServiceDemoTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.awsServiceDemoTest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsServiceDemoTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsServiceDemoTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsServiceDemoTest2/AwsServiceDemoTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsServiceDemoTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsServiceDemoTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
